f7fdccfb9e06d2058e97808bf04aa41d02eabacf
DECERLE <elodie.decerle@etu.univ-paris-diderot.fr>
2016-02-19 18:22:40 +0100

    Add MainTactic

---
+++ EV3_Bot/src/strategy/MainTactic.java
@@ -0,0 +1,49 @@
+package strategy;
+
+import sensors.BasicColor;
+
+public class MainTactic implements Tactic {
+	PincerTactic pincerTactic;
+	Tactic mainTactic;
+	
+	MainTactic() {
+		pincerTactic = new PincerTactic();
+		mainTactic = new FollowLine(BasicColor.Red, BasicColor.Blue);
+	}
+	
+	@Override
+	public String getDisplayName() {
+		return mainTactic.getDisplayName();
+	}
+
+	@Override
+	public boolean handleObstacle() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean handleContact() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean perform() {
+		pincerTactic.perform();
+		return mainTactic.perform();
+	}
+
+	@Override
+	public void abort() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
