df928b16ee844adc04889da7223b5d657a83884d
emma <emma.lerandy@gmail.com>
2016-03-04 15:37:08 +0100

    Create GoBack and edit FollowLine

--- EV3_Bot/src/strategy/FollowLine.java
+++ EV3_Bot/src/strategy/FollowLine.java
@@ -3,18 +3,18 @@ package strategy;
 import config.DefaultPorts;
 import main.Bot;
 import sensors.BasicColor;
-import sensors.ColorDetector;
 
-public class FollowLine implements Tactic{
+public class FollowLine implements Tactic {
 	private AvoidFoe avoidfoe;
+	private GoBack turn;
 	private BasicColor c;
 	private BasicColor stopColor;
-	
-	public FollowLine (BasicColor color, BasicColor stop){
+
+	public FollowLine(BasicColor color, BasicColor stop) {
 		c = color;
 		stopColor = stop;
 	}
-	
+
 	@Override
 	public String getDisplayName() {
 		return "FollowLine";
@@ -28,13 +28,13 @@ public class FollowLine implements Tactic{
 
 	@Override
 	public boolean handleContact() {
-		// TODO Auto-generated method stub
-		return false;
+		turn = new GoBack();
+		return true;
 	}
 
 	@Override
 	public boolean perform() {
-		if(avoidfoe != null) {
+		if (avoidfoe != null) {
 			boolean result = avoidfoe.perform();
 			if (result)
 				avoidfoe = null;
@@ -42,32 +42,32 @@ public class FollowLine implements Tactic{
 		BasicColor curColor = Bot.getSensorsCache().getColor();
 		DefaultPorts.getRightMotor().forward();
 		DefaultPorts.getLeftMotor().forward();
-		if(curColor == stopColor){
-			//stop yeti
-			DefaultPorts.getLeftMotor().setSpeed(0);	
+		if (curColor == stopColor) {
+			// stop yeti
+			DefaultPorts.getLeftMotor().setSpeed(0);
 			DefaultPorts.getRightMotor().setSpeed(0);
-			return true;	
-		}else{
-			//turn left to find the searched color
+			return true;
+		} else {
+			// turn left to find the searched color
 			int dist = Bot.getSensorsCache().getLineDistance(c);
-			int baseSpeed = 360, speedo = 120; 
-			DefaultPorts.getLeftMotor().setSpeed(baseSpeed+speedo*dist/100);
-			DefaultPorts.getRightMotor().setSpeed(baseSpeed+speedo*(100-dist)/100);
+			int baseSpeed = 360, speedo = 120;
+			DefaultPorts.getLeftMotor().setSpeed(baseSpeed + speedo * dist / 100);
+			DefaultPorts.getRightMotor().setSpeed(baseSpeed + speedo * (100 - dist) / 100);
 		}
-		
+
 		return false;
 	}
 
 	@Override
 	public void abort() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void stop() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
---
+++ EV3_Bot/src/strategy/GoBack.java
@@ -0,0 +1,54 @@
+package strategy;
+
+import motor.BasicMotion;
+
+public class GoBack implements Tactic {
+
+	private boolean wall;
+
+	public GoBack() {
+		this.wall = true;
+	}
+
+	@Override
+	public String getDisplayName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean handleObstacle() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean handleContact() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * The robot need to back off before chose another strategy.
+	 */
+	@Override
+	public boolean perform() {
+		BasicMotion.rotate(180);
+		BasicMotion.moveBy(10);
+		this.wall = false;
+		return true;
+	}
+
+	@Override
+	public void abort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
