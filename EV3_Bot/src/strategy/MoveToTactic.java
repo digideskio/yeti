dc1551cbbf9f74ac8566aacc5b4d1e0e7ec83d9e
emma <emma.lerandy@gmail.com>
2016-04-11 12:48:27 +0200

    Update GoBack and MoveToTactic strategies

--- EV3_Bot/src/strategy/MoveToTactic.java
+++ EV3_Bot/src/strategy/MoveToTactic.java
@@ -1,80 +1,95 @@
 package strategy;
 
+import gps.PositionTracker;
 import main.Bot;
 import motor.BasicMotion;
 import motor.StraightMotion;
-import gps.PositionTracker;
 
 public class MoveToTactic implements Tactic {
+	private AvoidFoe avoidfoe;
+	private GoBack goback;
 	private boolean abort = false;
 	PositionTracker gps;
 	StraightMotion sm;
 	int targetx, targety;
 	int startx, starty, distance;
 	boolean rotated; // If we overshoot a little, we don't want to turn around
-	
+
 	MoveToTactic(int targetx, int targety) {
 		gps = Bot.getGPS();
 		rotated = false;
-		
+
 		this.targetx = targetx;
 		this.targety = targety;
 		startx = gps.getRawX();
 		starty = gps.getRawY();
 		int xdiff = targetx - startx, ydiff = targety - starty;
-		distance = (int) Math.sqrt(xdiff*xdiff + ydiff*ydiff);
-		
+		distance = (int) Math.sqrt(xdiff * xdiff + ydiff * ydiff);
+
 		sm = new StraightMotion();
 	}
 
 	@Override
 	public String getDisplayName() {
-		return "MoveTo"+targetx+";"+targety;
+		return "MoveTo" + targetx + ";" + targety;
 	}
 
 	@Override
 	public boolean handleObstacle() {
-		// TODO Auto-generated method stub
-		return false;
+		sm.stop();
+		avoidfoe = new AvoidFoe();
+		return true;
 	}
 
 	@Override
 	public boolean handleContact() {
-		// TODO Auto-generated method stub
-		return false;
+		sm.stop();
+		goback = new GoBack();
+		return true;
 	}
 
 	@Override
 	public boolean perform() {
+		if (avoidfoe != null) {
+			boolean result = avoidfoe.perform();
+			if (result)
+				avoidfoe = null;
+		}
+		if (goback != null) {
+			boolean res = goback.perform();
+			if (res)
+				goback = null;
+		}
+
 		if (abort) {
 			if (sm.isMoving())
 				sm.stop();
 			return true;
 		}
-		
+
 		if (sm.isMoving())
 			sm.updateGPS();
-		
+
 		if (!rotated) {
 			int xdiff = targetx - gps.getRawX(), ydiff = targety - gps.getRawY();
 			float rotDiff = gps.getOrientation().diff(xdiff, ydiff);
 			if (Math.abs(rotDiff) >= 1.) {
 				if (sm.isMoving())
 					sm.stop();
-				BasicMotion.rotate((int)rotDiff, true);
+				BasicMotion.rotate((int) rotDiff, true);
 			}
 			rotated = true;
 		}
-		
+
 		int xdiff = gps.getRawX() - startx, ydiff = gps.getRawY() - starty;
-		int curDist = (int) Math.sqrt(xdiff*xdiff + ydiff*ydiff);
-		
+		int curDist = (int) Math.sqrt(xdiff * xdiff + ydiff * ydiff);
+
 		if (curDist >= distance) {
 			if (sm.isMoving())
 				sm.stop();
 			return true;
 		}
-		
+
 		if (!sm.isMoving())
 			sm.start(true);
 
